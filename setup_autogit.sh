c28014930c816d61b8d4e12006186001cbe9d256
kbroughton <kbroughton@c12e.com>
2015-04-16 10:48:01 -0500

    ip-192-168-1-126.ec2.internal

--- setup_autogit.sh
+++ setup_autogit.sh
@@ -25,10 +25,10 @@ git branch --set-upstream-to=origin/live-edit live-edit
 git branch ${MY_EDITS_BRANCH}
 git branch --set-upstream-to=origin/$USER-edits $USER-edits
 
-echo ""***** Add source ~/.git_prompt.sh to .bash_profile"
+echo "***** Add source ~/.git_prompt.sh to .bash_profile *****"
 # Check if ~/.bash_profile already sources sublimerc.sh
 source_git_prompt=`grep git_prompt ${HOME}/.bash_profile`
-echo "Adding 'source $HOME/.git_prompt.sh' to bash_profile"
+echo "***** Adding 'source $HOME/.git_prompt.sh' to bash_profile *****"
 if [[ $source_git_prompt == '' ]];then
     cp 
     echo "source $HOME/.git_prompt.sh" >> ${HOME}/.bash_profile
@@ -49,34 +49,34 @@ if [[ ${ST_PATH} != "" ]];then
             mkdir -p ${ST_CONF_DIR}
         fi
         if [[ ! -f ${ST_CONF_DIR}/sublimerc.sh ]];then
-            echo "Copying sublimerc.sh to $ST_CONF_DIR"
+            echo "***** Copying sublimerc.sh to $ST_CONF_DIR *****"
             cp sublimerc.sh ${ST_CONF_DIR}
         fi
         # Check if ~/.bash_profile already sources sublimerc.sh
         source_sublimerc=`grep sublimerc ${HOME}/.bash_profile`
-        echo "Adding 'source $ST_CONF_DIR/sublimerc.sh' to bash_profile"
+        echo "***** Adding 'source $ST_CONF_DIR/sublimerc.sh' to bash_profile *****"
         if [[ $source_sublimerc == '' ]];then
             echo "source sublimerc.sh" >> ${HOME}/.bash_profile
         fi
 
-        echo "Installing sublime plugins"
+        echo "***** Installing sublime plugins *****"
         if [[ -f "${ST2_PACKAGES_DIR}" ]];then
             pushd "${ST2_PACKAGES_DIR}"
         
-            echo "Installing sublime autosave plugin"
+            echo "***** Installing sublime autosave plugin *****"
             git clone https://github.com/jamesfzhang/auto-save.git
             # Check if line exists in User/Default (OSX).sublime-keymap
             KEYMAP_LINE="{ "keys": ["ctrl+shift+s"], "command": "auto_save" }"
             popd
         elif [[ -f "${ST3_PACKAGES_DIR}" ]];then
             pushd ${ST3_PACKAGES_DIR}
-            echo "Installing sublime autosave plugin"
+            echo "***** Installing sublime autosave plugin *****"
             git clone https://github.com/jamesfzhang/auto-save.git
             # Check if line exists in User/Default (OSX).sublime-keymap
             KEYMAP_LINE="{ "keys": ["ctrl+shift+s"], "command": "auto_save" }"
             popd
         else 
-            echo "Did not find sublimetext 2 or 3 at $ST2_PACKAGES_DIR or $ST3_PACKAGES_DIR"
+            echo "***** Did not find sublimetext 2 or 3 at $ST2_PACKAGES_DIR or $ST3_PACKAGES_DIR *****"
             exit 1
         fi
 
