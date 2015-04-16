60f7f86e71fb3d955834de0ae9976fc1090f55ea
kbroughton <kbroughton@c12e.com>
2015-04-16 08:37:45 -0500

    only push/pull when on live-edit branch

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,9 @@
-git pull
-echo q >> testfile
-git add testfile
-git commit -am "kbroughton"
-git push origin/live-edit
-git push origin/$USER-edits
+BRANCH=`git branch | grep '*' | cut -f2 -d' '`
+if [[ "$BRANCH" == 'live-edit' ]];then
+    git pull
+    echo q >> testfile
+    git add testfile
+    git commit -am "kbroughton"
+    git push origin/live-edit
+    git push origin/$USER-edits
+fi
