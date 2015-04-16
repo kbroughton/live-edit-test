fe0fa4f4a47415def12732d8a23b363a49018fc0
kbroughton <kbroughton@c12e.com>
2015-04-16 10:25:02 -0500

    kbroughton

--- autogit.sh
+++ autogit.sh
@@ -1,9 +1,12 @@
 BRANCH=`git branch | grep '*' | cut -f2 -d' '`
 if [[ "$BRANCH" == 'live-edit' ]];then
+    echo "On branch live-edit"
     git pull
     echo q >> testfile
     git add testfile
     git commit -am "kbroughton"
     git push origin live-edit
     git push origin $USER-edits
+else
+    echo "Not on branch live-edit.  Doing nothing."
 fi
