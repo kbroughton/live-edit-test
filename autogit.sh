0a9b741ee20e7f64c52665d246a8ab56bd24c96c
kbroughton <kbroughton@c12e.com>
2015-04-16 10:27:01 -0500

    ip-192-168-1-126.ec2.internal

--- autogit.sh
+++ autogit.sh
@@ -1,11 +1,15 @@
 BRANCH=`git branch | grep '*' | cut -f2 -d' '`
 if [[ "$BRANCH" == 'live-edit' ]];then
     echo "On branch live-edit"
+    echo "**** git pull ***"
     git pull
     echo q >> testfile
+    echo "git add and git commit"
     git add testfile
-    git commit -am "kbroughton"
+    git commit -am "`hostname`"
+    echo "git push origin live-edit"
     git push origin live-edit
+    echo "git push origin $USER-edits"
     git push origin $USER-edits
 else
     echo "Not on branch live-edit.  Doing nothing."
